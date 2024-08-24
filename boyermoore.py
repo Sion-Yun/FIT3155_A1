d96871dec6e9eb0ca025b886ecc99e24d1b00f04
sion yun <syun0003@student.monash.edu>
2024-08-24 21:24:41 +0800

    A1 Q2 - Z algorithm added - Wildcard approach in progress

--- boyermoore.py
+++ boyermoore.py
@@ -3,6 +3,8 @@ __author__ = "Yun Sion"
 
 import sys
 
+ALPHABET_SIZE = 256
+
 
 def z_algo(txt) -> [int]:
     n = len(txt)  # input string
@@ -33,16 +35,13 @@ def z_algo(txt) -> [int]:
                 r -= 1
     return z
 
-
-def bad_character(pat:str):
+def bad_char(pat):
     pass
 
-
-def good_suffix(pat: str):
+def good_suffix(pat):
     pass
 
-
-def matched_prefix(pat: str):
+def matched_prefix(pat):
     pass
 
 
@@ -58,31 +57,8 @@ def boyer_moore(txt: str, pat:str) -> [int]:
         pat (str): The pattern to match.
     :return: the positions of all instances of the pattern observed in that text.
     """
-
     # TODO - scan right to left + galil's optimisation
     # TODO - bad character, good suffix, matched prefix
-
-    """
-    The following tasks are performed by preprocessing the pattern and assuming a fixed size alphabet and input
-    pat[1 . . . m], can be completed in O(m) time:
-    • Preprocess the R(x) (Rk(x)) values for the bad character (extended bad character) rule.
-    • Preprocess the gs and mp values needed for the good suffix rule.
-    """
-
-    """
-    Given inputs pat[1 . . . m] and txt[1 . . . m] begin by aligning pat[1 . . . m] with txt[1 . . . m]. The algorithm
-    proceeds as follows:
-    1. Compare the current alignment in a right-to-left scan, applying Galil’s optimisation to terminate the
-    scan prematurely if appropriate.
-    2. After the scan has terminated due to a mismatch, or an occurrence of the pattern being found (which
-    is output appropriately):
-    (a) Use the (extended) bad character rule to determine how many places to the right pat should be
-    shifted under txt. Call this amount nbadcharacter.
-    (b) Use the good suffix and the matched prefix rules to determine how many places to the right pat
-    should be shifted under txt. Call this amount ngoodsuffix.
-    3. Shift pat to the right under txt by max(nbadcharacter, ngoodsuffix) places. Return to step 1.
-    """
-
     pass
 
 
@@ -93,4 +69,4 @@ if __name__ == '__main__':
 
     txt = 'ababcabc'
     pat = 'ab'
-    print(boyer_moore(txt, pat))
+    # print(boyer_moore(txt, pat))
